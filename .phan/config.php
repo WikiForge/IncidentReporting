cca541383aa845d03d7dc26014aa6b6defc17d33
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-12-12 11:12:29 -0700

    -

--- .phan/config.php
+++ .phan/config.php
@@ -2,8 +2,6 @@
 
 $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
 
-$cfg['target_php_version'] = '8.1';
-
 $cfg['suppress_issue_types'] = [
 	'SecurityCheck-LikelyFalsePositive',
 	'PhanPluginMixedKeyNoKey'
